bd8a22b42939d836c03006e731b392b27d398824
Dylan Van Assche <dylan.vanassche@ugent.be>
2024-06-12 14:43:16 +0200

    python: add Python bindings

--- build.sh
+++ build.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
 # Rust build
+echo "==> Rust building and bindings"
 cargo build
 
 # Java bindings
+echo "==> Java bindings"
+# Compile Translator CLI
 javac src/java/Translator.java
 
 # NodeJS bindings
+echo "==> NodeJS bindings"
+# Install NodeJS NEON dependencies
 npm i
+# Execute NEON on Rust library to generate index.node file for NodeJS dynamic library
 ./node_modules/.bin/neon dist -n translator -v -f target/debug/libltranslator.so
+# Move index.node to the right folder after generation
 mv index.node src/nodejs
+
+# Python bindings
+echo "==> Python bindings"
+# Native import, but requires renaming
+cp target/debug/libltranslator.so src/python/ltranslator.so
+
+echo "Done!"
--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,6 @@ pub mod api;
 pub mod util;
 mod java;
 mod nodejs;
+mod python;
 
 pub use java::*;
