efcc52e02fef3b7c9c4858e5a4cc109366d8e678
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2024-07-16 13:11:37 +0000

    Conditionally build bindings

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,15 @@ pub mod shexml;
 pub mod logger;
 pub mod api;
 pub mod util;
-mod java;
-mod nodejs;
-mod python;
 
+#[cfg(feature = "jni")]
+mod java;
+
+#[cfg(feature = "jni")]
 pub use java::*;
+
+#[cfg(feature = "neon")]
+mod nodejs;
+
+#[cfg(feature = "pyo3")]
+mod python;
