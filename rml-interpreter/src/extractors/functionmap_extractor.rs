9a6b74040d50933d154fd5c22fc17bb492b01b35
Sitt Min Oo <sittminoo1995145@gmail.com>
2025-01-28 15:32:14 +0100

    fix: Function map extractor unit test properly implemented

--- rml-interpreter/src/extractors/functionmap_extractor.rs
+++ rml-interpreter/src/extractors/functionmap_extractor.rs
@@ -55,7 +55,7 @@ impl Extractor<FunctionMap> for FunctionMap {
 }
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
+    use std::collections::{HashMap, HashSet};
     use std::fs::File;
     use std::io::BufReader;
     use std::path::PathBuf;
@@ -63,30 +63,59 @@ mod tests {
     use sophia_api::graph::Graph;
     use sophia_api::triple::Triple;
     use sophia_inmem::graph::FastGraph;
+    use sophia_term::literal::Literal;
+    use sophia_term::Term;
+
     use super::*;
     use crate::extractors::io::load_graph_bread;
     use crate::extractors::ExtractorResult;
     use crate::rml_model::source_target::SourceType;
+    use crate::rml_model::term_map::{ObjectMap, TermMapInfo};
     use crate::{load_graph, test_case};
 
     #[test]
     fn fno_function_test() -> ExtractorResult<()> {
-
         let graph: FastGraph = load_graph!("function_mapping.ttl")?;
 
-        // PredicateObjectMap predicate
-        let predicate_object_map_pred = vocab::r2rml::PROPERTY::PREDICATEOBJECTMAP.to_term();
-        let predicate_object_map_triple = graph.triples_with_p(&predicate_object_map_pred).next().unwrap()?;
-        let predicate_object_map_ref = predicate_object_map_triple.o();
+        // Function map IRI extraction
+        let predicate_object_map_pred =
+            vocab::fnml::PROPERTY::FUNCTION_VALUE.to_term();
+        let predicate_object_map_triple = graph
+            .triples_with_p(&predicate_object_map_pred)
+            .next()
+            .unwrap()?;
+        let function_map_ref = predicate_object_map_triple.o();
 
-        // ObjectMap predicate
-        let object_map_pred = vocab::r2rml::PROPERTY::OBJECTMAP.to_term();
-        let object_map_triple = graph.triples_with_o(predicate_object_map_ref).next().unwrap()?;
-        let object_map_ref = object_map_triple.o();
+        let function_map = FunctionMap::extract_self(function_map_ref, &graph)?;
+        assert_eq!(
+            function_map.function_iri,
+            "http://users.ugent.be/~bjdmeest/function/grel.ttl#toUpperCase"
+        );
+        let param_om_pair =
+            function_map.param_om_pairs.into_iter().next().unwrap();
 
-        // Extract the logical source from the ObjectMap
-        let logical_source = FunctionMap::extract_self(object_map_ref, &graph)?;
+        let expected_term_info = TermMapInfo {
+            identifier:      "".to_string(),
+            logical_targets: HashSet::new(),
+            term_map_type:   crate::rml_model::term_map::TermMapType::Reference,
+            term_value:      Term::new_literal_dt(
+                "Name",
+                vocab::xsd::TYPE::XSD_STRING.to_term(),
+            )?,
+            term_type:       Some(sophia_api::term::TermKind::Literal),
+            fun_map_opt:     None,
+        };
 
+        assert_eq!(
+            param_om_pair.0,
+            "http://users.ugent.be/~bjdmeest/function/grel.ttl#valueParameter"
+        );
+
+        let generated_om = param_om_pair.1; 
+
+        assert_eq!(generated_om.tm_info.term_value, expected_term_info.term_value); 
+        assert_eq!(generated_om.tm_info.term_map_type, expected_term_info.term_map_type); 
+        assert_eq!(generated_om.tm_info.term_type, expected_term_info.term_type); 
         Ok(())
     }
 }
