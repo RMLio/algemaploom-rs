457fad3dc41d76a906ccbae62eeac214b1f9107c
Sitt Min Oo <sittminoo1995145@gmail.com>
2025-07-14 14:45:31 +0200

    refactor: Use TermMapEnums instead of specific types of term maps

--- translator/src/new_rml/rml_model/v2/mod.rs
+++ translator/src/new_rml/rml_model/v2/mod.rs
@@ -1,7 +1,19 @@
+use crate::new_rml::rml_model::v2::core::expression_map::term_map::{
+    GraphMap, ObjectMap, PredicateMap, SubjectMap
+};
+
 pub mod core;
 pub mod fnml;
 pub mod io;
 pub mod lv;
+
+#[derive(Debug, Clone)]
+pub enum TermMapEnum {
+    SubjectMap(SubjectMap),
+    PredicateMap(PredicateMap),
+    ObjectMap(ObjectMap),
+    GraphMap(GraphMap), 
+}
 
 pub trait AttributeAliaser {
     fn alias_attribute(&self, alias: &str) -> Self;
