211e21bdda8a7f02687853d49d4fc0628191976e
Sitt Min Oo <sittminoo1995145@gmail.com>
2025-08-05 15:50:15 +0200

    chore: Remove unused code and clean up source extraction

--- translator/src/new_rml/extractors/source/mod.rs
+++ translator/src/new_rml/extractors/source/mod.rs
@@ -1,11 +1,3 @@
-//mod csvw_source;
-//mod file_source;
-//mod kafka_source;
-//mod rdb_source;
-//mod tcp_source;
-
-use std::collections::HashMap;
-
 use sophia_api::graph::MutableGraph;
 use sophia_api::prelude::Any;
 use sophia_api::term::{FromTerm, Term, TermKind};
@@ -13,32 +5,11 @@ use sophia_inmem::graph::FastGraph;
 
 use super::error::ParseError;
 use super::store::{get_objects, get_subgraph_subject};
-use super::{stringify_rcterm, Extractor, ExtractorResult, RcTerm};
+use super::{Extractor, ExtractorResult, RcTerm};
 use crate::new_rml::extractors::store::get_object;
 use crate::new_rml::extractors::FromVocab;
 use crate::new_rml::rml_model::v2::io::source::{Source, SourceKind};
 
-pub fn extract_parse_config(
-    dialect_subject: &RcTerm,
-    graph: &FastGraph,
-    predicates: &[(String, RcTerm)],
-) -> ExtractorResult<HashMap<String, String>> {
-    let mut result = HashMap::new();
-    let _ = predicates.iter().try_for_each(
-        |(key, config_pred)| -> ExtractorResult<()> {
-            // Retrieve the config value for the subject-predicate pair
-            let config_val = get_object(graph, dialect_subject, config_pred);
-
-            if let Ok(val) = config_val {
-                result.insert(key.to_string(), stringify_rcterm(val).unwrap());
-            }
-
-            Ok(())
-        },
-    );
-
-    Ok(result)
-}
 impl Extractor<Source> for Source {
     fn extract_self<TS>(
         subject_ref: TS,
@@ -135,40 +106,3 @@ where
     })
 }
 
-//fn old_config_extract(
-//    subject: &RcTerm,
-//    graph: &FastGraph,
-//    match_result: RcTerm,
-//) -> Result<HashMap<String, String>, ParseError> {
-//    let config = match match_result {
-//        iri_string if iri_string == vocab::csvw::CLASS::TABLE.to_rcterm() => {
-//            csvw_source::extract_csvw_source(subject, graph)
-//        }
-//
-//        iri_string
-//            if iri_string == vocab::d2rq::CLASS::DATABASE.to_rcterm() =>
-//        {
-//            rdb_source::extract_rdb_source(subject, graph)
-//        }
-//        iri_string
-//            if iri_string == vocab::rmls::CLASS::KAFKASTREAM.to_rcterm() =>
-//        {
-//            kafka_source::extract_kafka_source(subject, graph)
-//        }
-//
-//        iri_string
-//            if iri_string
-//                == vocab::rmls::CLASS::TCPSOCKETSTREAM.to_rcterm() =>
-//        {
-//            tcp_source::extract_tcp_source(subject, graph)
-//        }
-//
-//        invalid_iri => {
-//            Err(ParseError::GenericError(format!(
-//                "Source type extraction not yet supported {:#?}",
-//                invalid_iri
-//            )))
-//        }
-//    }?;
-//    Ok(config)
-//}
