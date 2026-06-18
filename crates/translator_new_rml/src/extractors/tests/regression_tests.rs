b5b843da1a0619a12058d920378a3da8b6f6c2f7
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2026-06-18 13:09:17 +0200

    RML-Core regression tests: added some tests + use a trick to compare plans over different runs.

--- crates/translator_new_rml/src/extractors/tests/regression_tests.rs
+++ crates/translator_new_rml/src/extractors/tests/regression_tests.rs
@@ -1,6 +1,7 @@
 use crate::translator::NewRMLDocumentTranslator;
 use std::error::Error;
 use std::fs;
+use std::hash::{DefaultHasher, Hash, Hasher};
 use std::path::PathBuf;
 use translator_api::LanguageTranslator;
 
@@ -19,6 +20,32 @@ fn rmltc0001b_json() -> Result<(), Box<dyn Error>> {
     execute("RMLTC0001b-JSON", true)
 }
 
+#[test]
+fn rmltc0002a_json() -> Result<(), Box<dyn Error>> {
+    execute("RMLTC0002a-JSON", true)
+}
+
+#[test]
+fn rmltc0002b_json() -> Result<(), Box<dyn Error>> {
+    execute("RMLTC0002b-JSON", true)
+}
+
+#[test]
+fn rmltc0002e_json() -> Result<(), Box<dyn Error>> {
+    execute("RMLTC0002e-JSON", true)
+}
+
+#[test]
+fn rmltc0002g_json() -> Result<(), Box<dyn Error>> {
+    execute("RMLTC0002g-JSON", true)
+}
+
+#[test]
+fn rmltc0003c_json() -> Result<(), Box<dyn Error>> {
+    execute("RMLTC0003c-JSON", true)
+}
+
+
 fn execute(test_name: &str, must_succeed: bool) -> Result<(), Box<dyn Error>> {
     let test_path = test_path(test_name);
     let mapping_path = test_path.join("mapping.ttl");
@@ -28,13 +55,12 @@ fn execute(test_name: &str, must_succeed: bool) -> Result<(), Box<dyn Error>> {
             panic!("A plan was generated but the test was expected to fail");
         }
         let actual_json_plan = plan.to_json_string()?;
-        let actual_plan: serde_json::Value = serde_json::from_str(&actual_json_plan)?;
+        let actual_plan_hash = json_hash(&actual_json_plan);
 
         let expected_json_path = test_path.join("mapping.json");
-        let expected_json_plan = fs::read_to_string(expected_json_path)?;
-        let expected_plan: serde_json::Value = serde_json::from_str(&expected_json_plan)?;
-
-        assert_eq!(actual_plan, expected_plan);
+        let expected_json_str = fs::read_to_string(expected_json_path)?;
+        let expected_plan_hash = json_hash(&expected_json_str);
+        assert_eq!(actual_plan_hash, expected_plan_hash, "Generated plan did not match expected plan.\nActual:\n{}\nExpected:\n{}", actual_json_plan, expected_json_str);
     } else {
         if must_succeed {
             panic!("No plan was generated but the test was expected to pass");
@@ -42,6 +68,20 @@ fn execute(test_name: &str, must_succeed: bool) -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+/// Deserializes a JSON string to a JSON object and then serializes it back to a string.
+/// Then the hash of this string is returned.
+/// This workaround catches the case where comparing JSON objects is not enough,
+/// e.g. if strings within a template are switched between different runs.
+fn json_hash(json_str: &str) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    let value = serde_json::from_str::<serde_json::Value>(json_str).unwrap();
+    let unsorted_str = serde_json::to_string(&value).unwrap();
+    let mut char_str = unsorted_str.chars().collect::<Vec<char>>();
+    char_str.sort();
+    char_str.hash(&mut hasher);
+    hasher.finish()
 }
 
 #[inline]
